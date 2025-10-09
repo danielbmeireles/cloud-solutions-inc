dc08b484c1592768a8fdb0eadae44151c8fe99f2
Daniel Meireles <16562270+danielbmeireles@users.noreply.github.com>
2025-10-09 23:00:01 +0100

    fix(kubernetes): 🐛 add placeholder values for Kubernetes provider
    * Add placeholder values for the Kubernetes and Helm providers
    * Now, the EKS module is responsible to configure both providers

--- versions.tf
+++ versions.tf
@@ -29,25 +29,18 @@ provider "aws" {
   }
 }
 
-# Data source for EKS cluster authentication
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  alias                  = "k8s_placeholder"
+  host                   = "https://placeholder"
+  cluster_ca_certificate = base64decode("placeholder")
+  token                  = "placeholder"
 }
 
 provider "helm" {
+  alias = "helm_placeholder"
   kubernetes = {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    host                   = "https://placeholder"
+    cluster_ca_certificate = base64decode("placeholder")
+    token                  = "placeholder"
   }
 }
