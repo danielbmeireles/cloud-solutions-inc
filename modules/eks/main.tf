408f043595a8cffcb27d0cb108ce5e53dec29a4c
Daniel Meireles <16562270+danielbmeireles@users.noreply.github.com>
2025-10-09 23:00:23 +0100

    feat(eks): ✨ add EKS cluster authentication and Kubernetes provider configuration
    * Implemented a workaround for provider/module limitations.
    * Added `aws_eks_cluster_auth` data source for EKS cluster authentication.
    * Configured Kubernetes and Helm providers to use the EKS cluster details.

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -458,3 +458,22 @@ resource "helm_release" "aws_load_balancer_controller" {
 
 # Data  Source to get current region
 data "aws_region" "current" {}
+
+# "Gambiarra" técnica - Brazilian-style workaround for provider/module limitations
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.main.name
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.id
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = aws_eks_cluster.main.id
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
