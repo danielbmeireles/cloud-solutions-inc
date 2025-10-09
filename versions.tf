cbf8f64cf2c5ce46987c1b71eaadee0d9fc996c6
Daniel Meireles <16562270+danielbmeireles@users.noreply.github.com>
2025-10-09 20:41:53 +0100

    feat(terraform): ✨ update provider versions and add EKS data sources
    * Updated `aws` provider version to `~> 6.0`.
    * Added data sources for EKS cluster authentication.
    * Configured `kubernetes` and `helm` providers to use EKS cluster details.

--- versions.tf
+++ versions.tf
@@ -4,7 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
     }
   }
 }
@@ -18,5 +26,28 @@ provider "aws" {
       ManagedBy   = "Terraform"
       Environment = var.environment
     }
+  }
+}
+
+# Data source for EKS cluster authentication
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
