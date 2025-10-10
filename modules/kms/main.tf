3f46334ab86bd3ad2dfc2543c8caed75bc9027d2
Daniel Meireles <16562270+danielbmeireles@users.noreply.github.com>
2025-10-10 14:04:13 +0100

    refactor(kms): 🏭️ move data sources to a new file
    * Moved the data sources for `aws_caller_identity` and `aws_region` from `main.tf` to a new file `data.tf` for better organization.

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -170,9 +170,3 @@ resource "aws_kms_alias" "ebs" {
   name          = "alias/${var.project_name}-${var.environment}-ebs"
   target_key_id = aws_kms_key.ebs.key_id
 }
-
-# Data source to get current account ID
-data "aws_caller_identity" "current" {}
-
-# Data source to get current region
-data "aws_region" "current" {}
